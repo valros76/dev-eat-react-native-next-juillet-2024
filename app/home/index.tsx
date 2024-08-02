9b0cf3d68d94f40bf335ccc02aa5b914c6b0cb22
valros76 <valerian.dufrene@gmail.com>
2024-08-02 10:37:40 +0200

    Code du 02-08-2024, à 10h37

---
+++ app/home/index.tsx
@@ -0,0 +1,40 @@
+import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
+
+
+export default function HomeScreen(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Dev-Eat</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://images.unsplash.com/photo-1536523218403-e72792eb06a1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }}
+      />
+      <Text>28 Rue d'Avron, 75020 Paris</Text>
+      <Text>Lundi : Fermé</Text>
+      <Text>Mardi : 12h30 - 15h00 / 18h30 - 21h30</Text>
+      <Text>Mercredi : 12h30 - 15h00 / 18h30 - 21h30</Text>
+      <Text>Jeudi : Fermé</Text>
+      <Text>Vendredi : 12h30 - 15h00 / 18h30 - 21h30</Text>
+      <Text>Samedi : 12h30 - 15h00 / 18h30 - 21h30</Text>
+      <Text>Dimanche : 12h30 - 15h00 / 18h30 - 21h30</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:"flex-start",
+    alignItems:"center",
+  },
+  title:{
+    fontSize:24,
+    fontWeight:"bold",
+  },
+  image:{
+    width:256,
+    height:128,
+  }
+});
