7a3a8e4e26153fb74294b15cd8e894e2e4f7efdb
valros76 <valerian.dufrene@gmail.com>
2024-08-02 11:13:43 +0200

    Code du 02-08-2024, à 11h13

---
+++ components/globals/Title.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text } from "react-native";
+
+
+export default function Title({children}){
+  return(
+    <Text style={styles.title}>
+      {children}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  title:{
+    fontSize:24,
+    fontWeight:"bold",
+  }
+});
