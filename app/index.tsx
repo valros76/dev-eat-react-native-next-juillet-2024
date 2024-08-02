19682bbc3665ecc4ac96cb415863bee19d349de8
valros76 <valerian.dufrene@gmail.com>
2024-08-02 14:04:43 +0200

    Code du 02-08-2024, à 14h04

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,59 @@
-import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { Title } from "@/components/globals";
-import { Localisation, Schedules } from "@/components/restaurant";
+import {
+  Localisation,
+  Schedules,
+} from "@/components/restaurant";
 
-export default function HomeScreen(){
-  return(
+export default function HomeScreen() {
+  return (
     <SafeAreaView style={styles.container}>
-      <Title>Dev-Eat</Title>
-      <Image
-        style={styles.image}
-        source={{
-          uri: "https://images.unsplash.com/photo-1536523218403-e72792eb06a1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }}
-      />
-      <Localisation />
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.mainContentView}>
+          <Title>Dev-Eat</Title>
+          <Image
+            style={styles.image}
+            source={{
+              uri: "https://images.unsplash.com/photo-1536523218403-e72792eb06a1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            }}
+          />
+          <Localisation />
 
-      <Schedules/>
+          <Schedules />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  scrollView: {
+    width: "100%",
+  },
+  mainContentView:{
     flex:1,
     justifyContent:"flex-start",
     alignItems:"center",
+    gap:12,
   },
-  title:{
-    fontSize:24,
-    fontWeight:"bold",
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
-  image:{
-    width:256,
-    height:128,
-  }
+  image: {
+    width: 256,
+    height: 128,
+  },
 });
