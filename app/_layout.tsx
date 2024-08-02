feefa4752ca2e7068184b971c9681a47ff392458
valros76 <valerian.dufrene@gmail.com>
2024-08-02 10:01:30 +0200

    Code du 02-08-2024, à 10h01

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,37 @@
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Tabs } from "expo-router";
+import { StyleSheet, Text } from "react-native";
 
 export default function RootLayout(){
   return(
     <Tabs>
       <Tabs.Screen
-        
+        name="home/index"
+        options={{
+          title:"Accueil",
+          tabBarLabel: () => (<Text>Accueil</Text>),
+          tabBarIcon: () => (<TabBarIcon name="home-outline" size={14}/>),
+          tabBarItemStyle: {
+            ...styles.tabBarItemCustom,
+            backgroundColor: "#FFFFFF"
+          },
+          tabBarIconStyle: {
+            ...styles.tabBarIconCustom
+          }
+        }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarItemCustom:{
+    flex:1,
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  tabBarIconCustom:{
+    height:18,
+  }
+});
