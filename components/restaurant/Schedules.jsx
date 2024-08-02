856d5bcaad0642da987d630c925bc28ce5e41790
valros76 <valerian.dufrene@gmail.com>
2024-08-02 13:51:56 +0200

    Code du 02-08-2024, à 13h51

--- components/restaurant/Schedules.jsx
+++ components/restaurant/Schedules.jsx
@@ -1,7 +1,7 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
 
-export default function Schedules({props = {}}){
+export default function Schedules({ props = {} }) {
 
   const { customDatas } = props;
 
@@ -22,49 +22,56 @@ export default function Schedules({props = {}}){
       day: "Mercredi",
       lunch: "11h30 - 13h30",
       dinner: "18h30 - 21h30",
-      isClose: false,
+      isClosed: false,
     },
     {
       day: "Jeudi",
       lunch: "",
       dinner: "",
-      isClose: true,
+      isClosed: true,
     },
     {
       day: "Vendredi",
       lunch: "11h30 - 15h30",
       dinner: "18h30 - 23h30",
-      isClose: false,
+      isClosed: false,
     },
     {
       day: "Samedi",
       lunch: "11h30 - 15h30",
       dinner: "18h30 - 23h30",
-      isClose: false,
+      isClosed: false,
     },
     {
       day: "Dimanche",
       lunch: "11h30 - 13h30",
       dinner: "18h30 - 21h30",
-      isClose: false,
+      isClosed: false,
     },
     {
       day: "Jours fériés",
       lunch: "11h30 - 14h30",
       dinner: "17h30 - 22h30",
-      isClose: false,
+      isClosed: false,
     }
   ];
 
-  return(
+  return (
     <View style={styles.container}>
       <FlatList
         data={customDatas || schedulesDatas}
         renderItem={(item) => (
           <>
             <Text style={styles.day}>{item.day}</Text>
-            <Text style={styles.schedules}>{item.lunch}</Text>
-            <Text style={styles.schedules}>{item.dinner}</Text>
+            {!item.isClosed && (
+              <>
+                <Text style={styles.schedules}>{item.lunch}</Text>
+                <Text style={styles.schedules}>{item.dinner}</Text>
+              </>
+            )}
+            {item.isClosed && (
+              <Text style={styles.schedules}>Fermé</Text>
+            )}
           </>
         )}
       />
@@ -74,20 +81,20 @@ export default function Schedules({props = {}}){
 
 const styles = StyleSheet.create({
   container: {
-    width:"100%",
-    justifyContent:"center",
-    alignItems:"stretch",
-    gap:8,
-    paddingVertical:8,
-    paddingHorizontal:16,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "stretch",
+    gap: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
   },
   day: {
     fontSize: 18,
     fontWeight: "bold",
-    letterSpacing:.66,
+    letterSpacing: .66,
   },
   schedules: {
-    fontSize:16,
-    letterSpacing:.33,
+    fontSize: 16,
+    letterSpacing: .33,
   }
 });
