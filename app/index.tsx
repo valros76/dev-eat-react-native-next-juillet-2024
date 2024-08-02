e071786668fd944b940186d634324d7b6190077d
valros76 <valerian.dufrene@gmail.com>
2024-08-02 15:30:08 +0200

    Code du 02-08-2024, à 15h30

--- app/index.tsx
+++ app/index.tsx
@@ -21,6 +21,7 @@ export default function HomeScreen() {
       <ScrollView style={styles.scrollView}>
         <View style={styles.mainContentView}>
           <Title>Dev-Eat</Title>
+          <Localisation />
           <Image
             style={{
               width: width,
@@ -30,7 +31,6 @@ export default function HomeScreen() {
               uri: "https://images.unsplash.com/photo-1536523218403-e72792eb06a1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             }}
           />
-          <Localisation />
 
           <Schedules />
         </View>
---
+++ app/restaurant-card/index.tsx
@@ -0,0 +1,21 @@
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+
+
+export default function RestaurantCardScreen(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+  scrollView: {
+    width:"100%",
+  }
+});
