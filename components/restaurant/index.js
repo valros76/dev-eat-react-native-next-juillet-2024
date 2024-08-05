e3880605f6d4b59c024de903a7af26cb26b263ef
valros76 <valerian.dufrene@gmail.com>
2024-08-05 12:34:38 +0200

    Code du 05-08-2024, à 12h34

--- components/restaurant/index.js
+++ components/restaurant/index.js
@@ -1,9 +1,11 @@
 import Localisation from "./Localisation";
 import Schedules from "./Schedules";
 import RestaurantCard from "./RestaurantCard";
+import RestaurantCardInfos from "./RestaurantCardInfos";
 
 export {
   Localisation,
   Schedules,
   RestaurantCard,
+  RestaurantCardInfos,
 }
